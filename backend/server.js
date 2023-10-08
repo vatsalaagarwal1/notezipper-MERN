4c82740fc6387e96953d30fe794b5379098ca312
Vatsala Agarwal <agarwalvatsala2002@gmail.com>
2023-10-08 16:23:53 +0530

    backend created

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const notes = require("./data/notes");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API is running...");
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get("/api/notes/:id/:id2", (req, res) => {
+    const note = notes.find((n) => n._id === req.params.id);
+    console.log(req.params);
+});
+
+app.listen(5000, console.log("Server started on PORT 5000"));
