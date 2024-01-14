12f345b0e4f4e6743c8d86dc620142a006e0e23c
Vatsala Agarwal <agarwalvatsala2002@gmail.com>
2024-01-14 10:55:16 +0530

    user registration
    Registration API created

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const app = express();
 dotenv.config();
 connectDB();
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("API is running...");
@@ -16,11 +17,16 @@ app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
-app.get("/api/notes/:id", (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id);
-    res.send(note);
-});
+app.use('/api/users', userRoutes);
+// app.get("/api/notes/:id", (req, res) => {
+//     const note = notes.find((n) => n._id === req.params.id);
+//     res.send(note);
+// });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
